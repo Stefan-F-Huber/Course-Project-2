668d87801b231997cb8fc0ffdd61c755b3dfb8e9
Stefan-F-Huber <stefan.franz.huber@gmx.at>
2020-05-24 14:17:15 +0200

    Script Plot1-6

---
+++ Script.R
@@ -0,0 +1,101 @@
+#Reading Data
+NEI <- readRDS("C:/Users/Administrator/Documents/RFile/coursera/Exploratory Data Analsys/week 4/summarySCC_PM25.rds")
+SCC <- readRDS("C:/Users/Administrator/Documents/RFile/coursera/Exploratory Data Analsys/week 4/Source_Classification_Code.rds")
+
+#Question 1:
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
+
+
+#Question 2:
+
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+png('plot2.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+dev.off()
+
+#Question 3:
+
+library(ggplot2)
+
+subsetNEI  <- NEI[NEI$fips=="24510", ]
+
+aggregatedTotalByYearAndType <- aggregate(Emissions ~ year + type, subsetNEI, sum)
+
+
+
+png("plot3.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYearAndType, aes(year, Emissions, color = type))
+g <- g + geom_line() +
+        xlab("year") +
+        ylab(expression('Total PM'[2.5]*" Emissions")) +
+        ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24510") from 1999 to 2008')
+print(g)
+dev.off()
+
+#Question 4:
+
+
+NEISCC <- merge(NEI, SCC, by="SCC")
+
+library(ggplot2)
+coalMatches  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
+subsetNEISCC <- NEISCC[coalMatches, ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEISCC, sum)
+
+
+
+png("plot4.png", width=640, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+        xlab("year") +
+        ylab(expression('Total PM'[2.5]*" Emissions")) +
+        ggtitle('Total Emissions from coal sources from 1999 to 2008')
+print(g)
+dev.off()
+
+#Question 5: 
+
+library(ggplot2)
+
+subsetNEI <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+
+
+png("plot5.png", width=840, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+        xlab("year") +
+        ylab(expression('Total PM'[2.5]*" Emissions")) +
+        ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+print(g)
+dev.off()
+
+#Question 6:
+
+library(ggplot2)
+subsetNEI <- NEI[(NEI$fips=="24510"|NEI$fips=="06037") & NEI$type=="ON-ROAD",  ]
+
+aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year + fips, subsetNEI, sum)
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
+aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
+
+png("plot6.png", width=1040, height=480)
+g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
+g <- g + facet_grid(. ~ fips)
+g <- g + geom_bar(stat="identity")  +
+        xlab("year") +
+        ylab(expression('Total PM'[2.5]*" Emissions")) +
+        ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
+print(g)
+dev.off()
